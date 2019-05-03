f1beaa912a6590f6e67541d79e869186bf509bf4
Josh Long <josh@joshlong.com>
2019-05-02 23:35:59 -0500

    up

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,6 +4,8 @@ APP_NAME=rmq-integration
 
 cf push -b java_buildpack --no-start -p target/integration-0.0.1-SNAPSHOT.jar ${APP_NAME}
 
+$ cf set-env ${APP_NAME} JBP_CONFIG_OPEN_JDK_JRE '{ jre: { version: 11.+ }, memory_calculator: { stack_threads: 25 } }'
+
 cf set-env $APP_NAME CF_API $CF_API
 cf set-env $APP_NAME CF_API_ENDPOINT $CF_API_ENDPOINT
 cf set-env $APP_NAME CF_ORG $CF_ORG
