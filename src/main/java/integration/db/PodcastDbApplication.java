cd36499bbc5f5bb11d6ace66e05bdb42caee9b8a
Josh Long <josh@joshlong.com>
2019-06-14 20:09:06 -0700

    polish

--- src/main/java/integration/db/PodcastDbApplication.java
+++ src/main/java/integration/db/PodcastDbApplication.java
@@ -49,30 +49,30 @@ class Initializer {
 		Podcast saved = this.template.execute(status -> {
 
 			var links = List.of(
-				Link.builder().description("Cornelia on Twitter")
-					.href("http://twitter.com/cfdavisaf").build(),
-				Link.builder().description("Cloud Native Patterns").href(
-					"https://www.manning.com/books/cloud-native-patterns")
-					.build());
+					Link.builder().description("Cornelia on Twitter")
+							.href("http://twitter.com/cfdavisaf").build(),
+					Link.builder().description("Cloud Native Patterns")
+							.href("https://www.manning.com/books/cloud-native-patterns")
+							.build());
 
-			var media = List.of(Media.builder().type(Media.TYPE_INTERVIEW)
-					.href("http://s3.com/media/cornelia_davis/2322-interview.wav")
-					.extension(Media.EXTENSION_WAV).build(),
-				Media.builder().type(Media.TYPE_INTRODUCTION).href(
-					"http://s3.com/media/cornelia_davis/2322-introduction.wav")
-					.extension(Media.EXTENSION_WAV).build());
+			var media = List.of(
+					Media.builder().type(Media.TYPE_INTERVIEW)
+							.href("http://s3.com/media/cornelia_davis/2322-interview.wav")
+							.extension(Media.EXTENSION_WAV).build(),
+					Media.builder().type(Media.TYPE_INTRODUCTION).href(
+							"http://s3.com/media/cornelia_davis/2322-introduction.wav")
+							.extension(Media.EXTENSION_WAV).build());
 
 			var podcast = Podcast.builder().media(media).links(links)
-				.title("Cornelia Davis on  her new book 'Cloud Native Patterns'")
-				.description(
-					"Join Pivotal SVP Cornelia Davis for a look at what it is to build cool things")
-				.transcript("TODO")
-				.notes("I couldn't find that photo I took of us together so I just used her Twitter profile picture (with her permission)")
-				.build();
+					.title("Cornelia Davis on  her new book 'Cloud Native Patterns'")
+					.description(
+							"Join Pivotal SVP Cornelia Davis for a look at what it is to build cool things")
+					.transcript("TODO")
+					.notes("I couldn't find that photo I took of us together so I just used her Twitter profile picture (with her permission)")
+					.build();
 
 			return repository.save(podcast);
 		});
-
 
 		log.info("saved " + Podcast.class.getName() + "#" + saved.getId());
 
@@ -121,14 +121,14 @@ class Podcast {
 
 	private Date date = new Date();
 
-	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
 	@JoinTable(name = "podcast_link", joinColumns = @JoinColumn(name = "podcast_id"),
-		inverseJoinColumns = @JoinColumn(name = "link_id"))
+			inverseJoinColumns = @JoinColumn(name = "link_id"))
 	private List<Link> links = new ArrayList<>();
 
-	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
 	@JoinTable(name = "podcast_media", joinColumns = @JoinColumn(name = "podcast_id"),
-		inverseJoinColumns = @JoinColumn(name = "media_id"))
+			inverseJoinColumns = @JoinColumn(name = "media_id"))
 	private List<Media> media = new ArrayList<>();
 
 }
