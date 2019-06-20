04c3630d65a3d827a76fff8ecc6ae9a4dc0d9b6e
Josh Long <josh@joshlong.com>
2019-06-20 00:55:46 -0500

    integration.

--- src/main/java/integration/aws/AwsConfiguration.java
+++ src/main/java/integration/aws/AwsConfiguration.java
@@ -5,11 +5,13 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import integration.PipelineProperties;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import integration.PipelineProperties;
 
+@Log4j2
 @Configuration
 class AwsConfiguration {
 
@@ -22,6 +24,10 @@ class AwsConfiguration {
 	AmazonS3 amazonS3(@Value("${aws_access_key_id}") String accessKey,
 			@Value("${aws_secret_access_key}") String secret,
 			@Value("${aws_region}") String region) {
+
+		log.info(accessKey);
+		log.info(secret);
+		log.info(region);
 		var credentials = new BasicAWSCredentials(accessKey, secret);
 		return AmazonS3ClientBuilder.standard()
 				.withCredentials(new AWSStaticCredentialsProvider(credentials))
