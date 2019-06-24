30865395ce7410ef5afe9dee4b449b43883f23b2
Josh Long <josh@joshlong.com>
2019-06-23 21:15:16 -0700

    up

--- src/main/java/integration/PipelineHttpController.java
+++ src/main/java/integration/PipelineHttpController.java
@@ -21,14 +21,15 @@ class PipelineHttpController {
 	private final File file;
 
 	private final PipelineService service;
+
 	private final PodcastRepository podcastRepository;
+
 	private final String processingMessage = "processing";
+
 	private final String finishedMessage = "finished processing";
 
-	PipelineHttpController(
-		PipelineProperties props,
-		PodcastRepository repository,
-		PipelineService service) {
+	PipelineHttpController(PipelineProperties props, PodcastRepository repository,
+			PipelineService service) {
 
 		this.file = FileUtils.ensureDirectoryExists(props.getS3().getStagingDirectory());
 		this.service = service;
@@ -38,36 +39,29 @@ class PipelineHttpController {
 	@GetMapping("/podcasts/{uid}/status")
 	ResponseEntity<?> getStatusForPodcast(@PathVariable String uid) {
 		Optional<Podcast> byUid = podcastRepository.findByUid(uid);
-		Optional<Map<?, ?>> response = byUid
-			.map(podcast -> {
-				if (null != podcast.getProductionArtifact()) {
-					return Map.of("media-url", podcast
-							.getProductionArtifact(),
-						"status", this.finishedMessage);
-				}
-				else {
-					return Map.of("status", this.processingMessage);
-				}
-			});
-		return response
-			.map(reply -> ResponseEntity.ok().body(reply))
-			.orElse(ResponseEntity.noContent().build());
+		Optional<Map<?, ?>> response = byUid.map(podcast -> {
+			if (null != podcast.getProductionArtifact()) {
+				return Map.of("media-url", podcast.getProductionArtifact(), "status",
+						this.finishedMessage);
+			}
+			else {
+				return Map.of("status", this.processingMessage);
+			}
+		});
+		return response.map(reply -> ResponseEntity.ok().body(reply))
+				.orElse(ResponseEntity.noContent().build());
 	}
 
 	@PostMapping("/podcasts/{uid}")
-	ResponseEntity<?> beginProduction(
-		@PathVariable("uid") String uid,
-		@RequestParam("file") MultipartFile file) throws Exception {
+	ResponseEntity<?> beginProduction(@PathVariable("uid") String uid,
+			@RequestParam("file") MultipartFile file) throws Exception {
 		var newFile = new File(this.file, uid);
 		file.transferTo(newFile);
 		FileUtils.assertFileExists(newFile);
 		Assert.isTrue(this.service.launchPipeline(uid, newFile), "the pipeline says no.");
 		var location = URI.create("/podcasts/" + uid + "/status");
 		log.info("sending status location as : '" + location + "'");
-		return ResponseEntity
-			.accepted()
-			.location(location)
-			.build();
+		return ResponseEntity.accepted().location(location).build();
 	}
 
 }
