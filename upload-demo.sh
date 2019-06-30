97eb480e9666344f5d085b2dda6c0e099ef2c61f
Josh Long <josh@joshlong.com>
2019-06-30 01:32:09 -0400

    changing the routes.

--- upload-demo.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# curl -F"file=@./mvnw" http://localhost:8080/s3
-curl -F"file=@/Users/joshlong/Desktop/pkg.zip" -XPOST "http://service-spontaneous-dingo.cfapps.io/production?uid=13232"
