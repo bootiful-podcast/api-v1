5f4f8377d326ec1236a6d60aa68ac6eff5c62d63
Josh Long <josh@joshlong.com>
2020-01-09 20:00:48 -0800

    We're now publishing to Podbean, too!

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,15 +5,24 @@ APP_NAME=service
 cf push -b java_buildpack --no-start -p target/api-0.0.1-SNAPSHOT.jar ${APP_NAME}
 
 cf set-env ${APP_NAME} JBP_CONFIG_OPEN_JDK_JRE: '{ jre: { version: 11.+}}'
-
+##
+## CloudFoundry
 cf set-env $APP_NAME CF_API $CF_API
 cf set-env $APP_NAME CF_API_ENDPOINT $CF_API_ENDPOINT
 cf set-env $APP_NAME CF_ORG $CF_ORG
 cf set-env $APP_NAME CF_PASSWORD $CF_PASSWORD
 cf set-env $APP_NAME CF_SPACE $CF_SPACE
 cf set-env $APP_NAME CF_USER $CF_USER
+##
+## RabbitMQ
 cf set-env $APP_NAME RMQ_ADDRESS $RMQ_ADDRESS
+##
+## Sendgrid
 cf set-env $APP_NAME SENDGRID_API_KEY $SENDGRID_API_KEY
+##
+## Podbean
+cf set-env $APP_NAME PODBEAN_CLIENT_ID $PODBEAN_CLIENT_ID
+cf set-env $APP_NAME PODBEAN_CLIENT_SECRET $PODBEAN_CLIENT_SECRET
 ##
 ## AWS
 cf set-env $APP_NAME AWS_SECRET_ACCESS_KEY $AWS_SECRET_ACCESS_KEY
